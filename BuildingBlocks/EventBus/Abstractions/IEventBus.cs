83e9de750b8c7df8eb123b1e2a2c1f9ccb35e091
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2020-08-29 15:19:48 +0300

    added basic subscribe implementation

--- BuildingBlocks/EventBus/Abstractions/IEventBus.cs
+++ BuildingBlocks/EventBus/Abstractions/IEventBus.cs
@@ -9,5 +9,7 @@ namespace EventBus.Abstractions
     {
         void Publish(IntegrationEvent @event);    
 
+        void Subscribe<T, TH>() where T: IntegrationEvent where TH: IIntegrationEventHandler<T>;
+
     }
 }
--- BuildingBlocks/RabbitMQEventBus/EventBusRabbitMQ.cs
+++ BuildingBlocks/RabbitMQEventBus/EventBusRabbitMQ.cs
@@ -3,6 +3,7 @@ using EventBus.Abstractions;
 using EventBus.Events;
 using Newtonsoft.Json;
 using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace RabbitMQEventBus
 {
@@ -10,8 +11,34 @@ namespace RabbitMQEventBus
     {
         private readonly string rabbitMQ_hostName = "localhost";
         private readonly string exchange_Name = "eshop_exchange";
+        private IModel _consumerChannel;
+        private string _queueName;
 
 
+        public EventBusRabbitMQ(string queueName)
+        {
+            _consumerChannel = CreateConsumerChannel();
+            _queueName = queueName;
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            var factory = new ConnectionFactory() { HostName = rabbitMQ_hostName };
+
+            var conn = factory.CreateConnection();
+
+            var channel = conn.CreateModel();
+            
+            channel.ExchangeDeclare(exchange: this.exchange_Name, ExchangeType.Fanout);
+
+            channel.QueueDeclare(queue: this._queueName,
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            return channel;
+        }
         public void Publish(IntegrationEvent @event)
         {
             var factory = new ConnectionFactory() { HostName = this.rabbitMQ_hostName };
@@ -29,6 +56,34 @@ namespace RabbitMQEventBus
                      body);
                 }
             }
+        }
+
+        public void Subscribe<T, TH>()
+            where T : IntegrationEvent
+            where TH : IIntegrationEventHandler<T>
+        {
+
+                var factory = new ConnectionFactory() { HostName = rabbitMQ_hostName };
+
+                using(var connection = factory.CreateConnection())
+                {
+                    this._consumerChannel.QueueBind(queue: _queueName,
+                        exchange: this.exchange_Name, routingKey: null);
+                }
+
+                //start basic consumer.
+
+                var consumer = new EventingBasicConsumer(_consumerChannel);
+                
+                _consumerChannel.BasicConsume(queue: this._queueName,
+                    autoAck: false,
+                    consumer: consumer);
+
+                consumer.Received += (model, ea) => {
+                    
+                    var x = 2;
+                };
+
         }
     }
 }
