779aa27d1d84b6c1824176c3297a104892c534c2
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2020-09-16 19:37:19 +0300

    removed mediatr impelementation (2)

--- Services/Order/Order.API/Application/IntegrationEvents/EventHandling/UserCheckoutAcceptedIntegrationEventHandler.cs
+++ Services/Order/Order.API/Application/IntegrationEvents/EventHandling/UserCheckoutAcceptedIntegrationEventHandler.cs
@@ -1,7 +1,5 @@
 using EventBus.Abstractions;
-using MediatR;
 using Microsoft.Extensions.Logging;
-using Order.API.Application.Command;
 using Order.API.Application.IntegrationEvents.Events;
 using System.Threading.Tasks;
 
@@ -10,25 +8,24 @@ namespace Order.API.Application.IntegrationEvents.EventHandling
     public class UserCheckoutAcceptedIntegrationEventHandler : IIntegrationEventHandler<UserCheckoutAcceptedIntegrationEvent>
     {
         private ILogger<UserCheckoutAcceptedIntegrationEventHandler> _logger;
-        private IMediator _mediator;
-        public UserCheckoutAcceptedIntegrationEventHandler(ILogger<UserCheckoutAcceptedIntegrationEventHandler> logger,
-            IMediator mediator)
+        public UserCheckoutAcceptedIntegrationEventHandler(ILogger<UserCheckoutAcceptedIntegrationEventHandler> logger)
         {
             this._logger = logger;
-            this._mediator = mediator;            
         }
+
         public Task Handle(UserCheckoutAcceptedIntegrationEvent @event)
         {
             _logger.LogInformation("\t [.] Inside the UserCheckoutAcceptedIntegrationEventHandler");
 
             //Burada CreateOrderCommand oluşturup, mediatr ile göndereceğiz.
+            /*
             var command = new CreateOrderCommand(@event.Basket.Items, userId: @event.UserId, userName: @event.UserName, city: @event.City,
                 street: @event.Street, state: @event.State, country: @event.Country, zipCode: @event.ZipCode,
                 cardNumber: @event.CardNumber, cardHolderName: @event.CardHolderName, cardExpiration: @event.CardExpiration,
                 cardSecurityNumber: @event.CardSecurityNumber, cardTypeId: @event.CardTypeId);
                 
-            _mediator.Send(command);
-
+            */
+            //TODO publish command
             return null;
         }
     }
