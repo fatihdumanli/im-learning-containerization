6b04adea3760d8db1a256ac7db0e743a640c999b
Fatih Tunahan Dumanli <fatihdumanli@live.com>
2020-09-27 22:44:34 +0300

    added configureContainer method to startup

--- Services/Payment/IntegrationEvents/EventHandling/OrderStatusChangedToStockConfirmedIntegrationEventHandler.cs
+++ Services/Payment/IntegrationEvents/EventHandling/OrderStatusChangedToStockConfirmedIntegrationEventHandler.cs
@@ -1,14 +1,23 @@
 using System.Threading.Tasks;
 using EventBus.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace Payment.IntegrationEvents
 {
     public class OrderStatusChangedToStockConfirmedIntegrationEventHandler :
         IIntegrationEventHandler<OrderStatusChangedToStockConfirmedIntegrationEvent>
     {
+        private ILogger<OrderStatusChangedToStockConfirmedIntegrationEventHandler> _logger;
+        
+        public OrderStatusChangedToStockConfirmedIntegrationEventHandler(ILogger<OrderStatusChangedToStockConfirmedIntegrationEventHandler> logger)
+        {
+            _logger = logger;
+        }
+
         public Task Handle(OrderStatusChangedToStockConfirmedIntegrationEvent @event)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation(" [x] OrderStatusChangedToStockConfirmedIntegrationEventHandler: Handling integration event..."); 
+            return null;
         }
     }
 }
--- Services/Payment/Startup.cs
+++ Services/Payment/Startup.cs
@@ -33,6 +33,12 @@ namespace Payment
         // This method gets called by the runtime. Use this method to add services to the container.
 
 
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<OrderStatusChangedToStockConfirmedIntegrationEventHandler>();
+        
+        }
+
 
         public void ConfigureServices(IServiceCollection services)
         {
